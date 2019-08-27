3f45b4fab286e98a281ae6e4bb6277f490b87abc
Chosen <qq4868@hotmail.com>
2019-08-27 15:04:02 +0800

    Initial Commit

---
+++ AVSpeech Practice/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AVSpeech Practice
+//
+//  Created by 邱士宸 on 2019/8/27.
+//  Copyright © 2019 Shih Chen Chiu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
